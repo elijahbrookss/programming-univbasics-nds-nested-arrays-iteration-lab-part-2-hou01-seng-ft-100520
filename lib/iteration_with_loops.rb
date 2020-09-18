fbd441cb44da2891e7e8216a6d2a13e01b1fb88f
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-18 19:48:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,7 @@
 def find_min_in_nested_arrays(src)
   for outerarray in src do
     minimumvalue = outerarray[0]
-    for innerarray in outerarry do
+    for innerarray in outerarray do
       if minimumvalue > innerarray then
         minimumvalue = innerarray
         p minimumvalue
